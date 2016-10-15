86d6169210b0e8c2c9dc1849bd727c49f77a1ebb
MilaBezaliuc <bezaliuc@gmail.com>
2016-10-15 17:49:14 +0300

    hw3

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS users CASCADE;
-DROP SEQUENCE IF EXISTS global_seq;
 DROP TABLE IF EXISTS meals;
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
 
@@ -22,14 +22,5 @@ CREATE TABLE user_roles
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
-  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
-
-CREATE TABLE meals
-(
-  date TIMESTAMP DEFAULT now(),
-  description VARCHAR NOT NULL,
-  calories INTEGER NOT NULL,
-  user_id INTEGER NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
