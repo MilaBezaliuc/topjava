db21a55ed125c400533e79dda2f67973ad63db21
MilaBezaliuc <bezaliuc@gmail.com>
2016-10-15 17:43:37 +0300

    db

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP TABLE IF EXISTS meals;
 
 CREATE SEQUENCE global_seq START 100000;
 
@@ -21,5 +22,14 @@ CREATE TABLE user_roles
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+  date TIMESTAMP DEFAULT now(),
+  description VARCHAR NOT NULL,
+  calories INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
